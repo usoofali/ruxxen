c93b1c07d141326f4930d0563dffb6c5863d8583
Aliyu Yusuf <usoofali@gmail.com>
2026-07-26 11:39:22 +0100

    assets

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -11,4 +11,17 @@
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+@if (file_exists(public_path('build/manifest.json')))
+    @php
+        $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+    @endphp
+    @if (isset($manifest['resources/css/app.css']['file']))
+        <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
+    @endif
+    @if (isset($manifest['resources/js/app.js']['file']))
+        <script type="module" src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}"></script>
+    @endif
+@endif
+
 @fluxAppearance
